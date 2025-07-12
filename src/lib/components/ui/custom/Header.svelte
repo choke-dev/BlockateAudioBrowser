83d6b2d18b57149e8dbedd30934f5092d3f672a0
choke-dev <admin@choke.dev>
2025-07-12 13:12:34 +0800

    fix: logo using the relative path instead of using the absolute path

--- src/lib/components/ui/custom/Header.svelte
+++ src/lib/components/ui/custom/Header.svelte
@@ -9,7 +9,7 @@
 <header class="flex items-center justify-between border-b px-8 py-2">
   <div>
     <a href="/">
-      <img src="./audiodb.png" class="size-10" alt="Blockate Audio Browser Logo" />
+      <img src="/audiodb.png" class="size-10" alt="Blockate Audio Browser Logo" />
     </a>
   </div>
   <div class="flex items-center gap-2 divide-x-2">
