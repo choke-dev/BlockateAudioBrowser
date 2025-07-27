abfee2f88bcb7a3dfdac987c1f1076b8bd395567
choke-dev <admin@choke.dev>
2025-07-27 09:57:23 +0800

    update page title on api docs

--- src/routes/api/docs/+server.ts
+++ src/routes/api/docs/+server.ts
@@ -2,6 +2,7 @@ import { ScalarApiReference } from '@scalar/sveltekit'
 import type { RequestHandler } from './$types'
 const render = ScalarApiReference({
   url: '/api/openapi.json',
+  pageTitle: "Blockate Audio Browser API (v1)"
 })
 
 export const GET: RequestHandler = () => {
