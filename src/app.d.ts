e1414aeb04d5d4b2e17fadf1d20a138f64674e65
choke-dev <admin@choke.dev>
2026-03-11 20:50:28 +0800

    chore: update packages

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,18 @@
 import 'unplugin-icons/types/svelte';
+import { users } from '$lib/server/db/schema';
+import type { InferSelectModel } from 'drizzle-orm';
+
+interface User {
+  id: string;
+  robloxId: string;
+  username: string;
+  avatar: string | null;
+  createdAt: string;
+  permissions: string[];
+}
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
+import type { Handle } from '@sveltejs/kit';
 // for information about these interfaces
 declare global {
 	namespace App {
